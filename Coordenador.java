7d6b911e55987fb7aab35ca11b36edf384c37bdd
Guilherme gg <ggdev0001@gmail.com>
2026-06-01 22:04:44 -0300

    inicio00

--- Coordenador.java
+++ Coordenador.java
@@ -1,3 +1,10 @@
+package Projeto-Afya-2up;
+
 public class Coordenador {
+
+    public Coordenador(String nome, String cpf, int idade, String email) {
+        super(nome, cpf, idade, email);
+        this.setor = setor;
+    }
 
 }
