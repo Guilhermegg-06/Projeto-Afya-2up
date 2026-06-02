433ab0e685431fc698f0e869afbd4c4d03f814ab
KevinAFS <kevinafsilva10@gmail.com>
2026-06-01 22:39:48 -0300

    Correção Coordenador2

--- Coordenador.java
+++ Coordenador.java
@@ -1,10 +1,17 @@
 package Projeto-Afya-2up;
 
-public class Coordenador {
+public class Coordenador extends Pessoa{
+
+    private static int proxIDCoord = 1;
+    private int iDCoord;
 
     public Coordenador(String nome, String cpf, int idade, String email) {
         super(nome, cpf, idade, email);
-        this.setor = setor;
+        this.iDCoord = proxIDCoord++;
+    }
+
+        public int getIdCoord() {
+        return iDCoord;
     }
 
 }
