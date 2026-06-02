47e4d35e9bbb71dd71169d7f3813c2a503217ca3
KevinAFS <kevinafsilva10@gmail.com>
2026-06-01 21:38:32 -0300

    Correção Aluno

--- Aluno.java
+++ Aluno.java
@@ -2,4 +2,18 @@ package Projeto-Afya-2up;
 
 public class Aluno {
 
+    private String matricula;
+
+    public Aluno(String nome, String cpf, int idade, String email, String matricula) {
+        super(nome, cpf, idade, email);
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 }
