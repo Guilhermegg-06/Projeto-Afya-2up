5a35b8de42c35e416c777f2d4365ea0de8793c68
KevinAFS <kevinafsilva10@gmail.com>
2026-06-05 22:21:10 -0300

    Adicionando ID nos Eventos, e mudança simples no construtor

--- Eventos.java
+++ Eventos.java
@@ -1,36 +1,43 @@
 public class Eventos {
 
+    private static int proxIdEvento = 1;
+    private int idEvento;
     private Boolean disponibilidadeEvento; /*Diz se o evento esta aberto, ou se ja fechou, seja por ja ter passado da Data ou pro escolha do organizador. */
     private String categoriaEvento;
     private String dataEvento;
     private String localEvento;
     private String descricaoEvento;
-    private String tipoEvento; /*Palestra ou MiniCurso */
+    private String tipoEvento; /*Palestra, MiniCurso e etc */
     private String nomeEvento;
     private Double horasComplementares;
     private int vagasDisponiveis;
-    private boolean presenca;
+    private Coordenador organizador;
 
-public Eventos(Boolean disponibilidadeEvento, String categoriaEvento, String dataEvento, String localEvento, String descricaoEvento, String tipoEvento, String nomeEvento, Double horasComplementares, int vagasDisponiveis, boolean presenca){
-    this.disponibilidadeEvento = disponibilidadeEvento;
-    this.categoriaEvento = categoriaEvento;
-    this.dataEvento = dataEvento;
-    this.localEvento = localEvento;
-    this.descricaoEvento = descricaoEvento;
-    this.tipoEvento = tipoEvento;
-    this.nomeEvento = nomeEvento;
-    this.horasComplementares = horasComplementares;
-    this.vagasDisponiveis = vagasDisponiveis;
-    this.presenca = presenca;
+    public Eventos(Boolean disponibilidadeEvento, String categoriaEvento, String dataEvento, String localEvento, String descricaoEvento, String tipoEvento, String nomeEvento, Double horasComplementares, int vagasDisponiveis, Coordenador organizador){
+        this.idEvento = proxIdEvento++;
+        this.disponibilidadeEvento = disponibilidadeEvento;
+        this.categoriaEvento = categoriaEvento;
+        this.dataEvento = dataEvento;
+        this.localEvento = localEvento;
+        this.descricaoEvento = descricaoEvento;
+        this.tipoEvento = tipoEvento;
+        this.nomeEvento = nomeEvento;
+        this.horasComplementares = horasComplementares;
+        this.vagasDisponiveis = vagasDisponiveis;
+        this.organizador = organizador;
+
 }
-    public boolean getdisponibilidadeEvento(){
+    public int getIdEvento() {
+        return idEvento;
+    }
+
+    public Boolean getDisponibilidadeEvento() {
         return disponibilidadeEvento;
     }
 
-    public void setdisponibilidadeEvento(boolean disponibilidadeEvento){
+    public void setDisponibilidadeEvento(Boolean disponibilidadeEvento) {
         this.disponibilidadeEvento = disponibilidadeEvento;
     }
-
 
     public String getCategoriaEvento() {
         return categoriaEvento;
@@ -96,12 +103,12 @@ public Eventos(Boolean disponibilidadeEvento, String categoriaEvento, String dat
         this.vagasDisponiveis = vagasDisponiveis;
     }
 
-    public boolean isPresenca() {
-        return presenca;
+    public Coordenador getOrganizador() {
+        return organizador;
     }
 
-    public void setPresenca(boolean presenca) {
-        this.presenca = presenca;
+    public void setOrganizador(Coordenador organizador) {
+        this.organizador = organizador;
     }
 
 }
