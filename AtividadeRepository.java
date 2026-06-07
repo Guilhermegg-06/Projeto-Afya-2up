3ad7d0e517b208c9c5b6e1b3f7d5aa373fdd280e
KevinAFS <kevinafsilva10@gmail.com>
2026-06-07 20:45:52 -0300

    Update AtividadeRepository

--- AtividadeRepository.java
+++ AtividadeRepository.java
@@ -1,3 +1,9 @@
-public interface AtividadeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AtividadeRepository
+        extends JpaRepository<Atividade, Long> {
+
+    List<Atividade> findByEventoId(Long eventoId);
 
 }
